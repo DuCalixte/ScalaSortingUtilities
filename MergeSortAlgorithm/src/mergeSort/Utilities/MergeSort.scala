3d40777295ec7fae73312d8128e43fa8d3ef18c4
Stanley Calixte <DuCalixte@gmail.com>
2013-08-05 22:01:15 -0400

    Optimizing merge sort with Stream

--- MergeSortAlgorithm/src/mergeSort/Utilities/MergeSort.scala
+++ MergeSortAlgorithm/src/mergeSort/Utilities/MergeSort.scala
@@ -43,7 +43,7 @@ object MergeSort {
     
   }
   
-  def mergeSort[T <% Ordered[T]:ClassManifest](list:List[T]):List[T]= list match{
+  def standardmergeSort[T <% Ordered[T]:ClassManifest](list:List[T]):List[T]= list match{
     case List() => list
     case single::Nil => list
     case _ =>
@@ -56,6 +56,21 @@ object MergeSort {
       }
       val(left,right) = list.splitAt(list.length/2)
       merge(mergeSort(left), mergeSort(right))
+  }
+  
+  def mergeSort[T <% Ordered[T]:ClassManifest](list:List[T]):List[T]= list match{
+    case Nil | List(_) => list
+    case _ =>
+      def merge[T <% Ordered[T]:ClassManifest](left: List[T], right: List[T]): Stream[T] = (left, right) match {
+        case (l :: _left, r :: _right) if l <= r => l #:: merge(_left, right)
+        case (left, r :: _right) => r #:: merge(left, _right)
+        case (left, Nil)=>left.toStream
+        case (Nil,right)=>right.toStream
+        case _ => Stream()
+      }
+      val split = list.length /2
+      val (left, right) = list splitAt split
+      merge(mergeSort(left), mergeSort(right)).toList
   }
 
 }
